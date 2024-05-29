781a2868d26942082d4c80ea728e83066d341673
kolinabir <knkolin9@gmail.com>
2024-05-30 02:58:39 +0600

    chore: Update package name and version to follow naming conventions

--- routes/jwtAuth.js
+++ routes/jwtAuth.js
@@ -1,11 +1,13 @@
 const router = require("express").Router();
 const db = require("../db/index");
+const bcrypt = require("bcrypt");
+const jwtGenerator = require("../utils/jwtGenarator");
 
 // register
 
 router.get("/", async (req, res) => {
   const result = await db.query("SELECT * FROM users");
-  console.log(result);
+
   res.status(200).json({
     status: "success",
     results: result.rows.length,
@@ -25,15 +27,51 @@ router.post("/register", async (req, res) => {
     if (user.rows.length !== 0) {
       return res.status(401).json("User already exists");
     }
+
+    //becrypt password
+    const saltRounds = 10;
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
     const newUser = await db.query(
       "INSERT INTO users (username, usermail, password) VALUES ($1, $2, $3) RETURNING *",
-      [username, usermail, password]
+      [username, usermail, hashedPassword]
     );
+
+    const token = jwtGenerator(newUser.rows[0].id);
     res.status(200).json({
       status: "success",
       data: {
         user: newUser.rows[0],
       },
+      token,
+    });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server error");
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const { usermail, password } = req.body;
+    const user = await db.query("SELECT * FROM users WHERE usermail = $1", [
+      usermail,
+    ]);
+    if (user.rows.length === 0) {
+      return res.status(401).json("Password or Email is incorrect");
+    }
+    const validPassword = await bcrypt.compare(password, user.rows[0].password);
+    if (!validPassword) {
+      return res.status(401).json("Password or Email is incorrect");
+    }
+    const token = jwtGenerator(user.rows[0].id);
+    res.status(200).json({
+      status: "success",
+      data: {
+        user: user.rows[0],
+      },
+      token,
     });
   } catch (error) {
     console.error(error.message);
